8da5d092dff9257a805ef9c49084bcfc779d6471
김태우 <taewoo080401@gmail.com>
2026-04-01 19:52:19 +0900

    feat: something

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,20 @@ import App from "./App.tsx";
 import { AppStateProvider } from "@b1nd/aid-kit/app-state";
 import { BridgeProvider } from "@b1nd/aid-kit/bridge-kit/web";
 import { SafeAreaProvider } from "@b1nd/aid-kit/safe-area-provider";
+import { RouteProvider, Router } from "@b1nd/aid-kit/navigation";
+
+const routes = {
+  tabs: [{ path: "/", index: true, element: App }],
+  stacks: [],
+};
 
 createRoot(document.getElementById("root")!).render(
   <AppStateProvider>
     <BridgeProvider>
       <SafeAreaProvider>
-        <App />
+        <RouteProvider routes={routes}>
+          <Router routes={routes} />
+        </RouteProvider>
       </SafeAreaProvider>
     </BridgeProvider>
   </AppStateProvider>,
